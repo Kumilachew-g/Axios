f2e4f03dc2368929b55557e0f52675718988f4a1
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-08 20:24:41 +0300

    Import PersonAdd into app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import PersonList from './components/PersonList';
+import PersonAdd from './components/PersonAdd';
 
 function App() {
   return (
     <div className='App'>
+      <PersonAdd />
       <PersonList />
     </div>
   );
