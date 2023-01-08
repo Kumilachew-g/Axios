c5f036293c62c8494850242464e22a2c110e2d86
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-08 20:56:18 +0300

    Import PersonRemove into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import PersonList from './components/PersonList';
 import PersonAdd from './components/PersonAdd';
+import PersonRemove from './components/PersonRemove';
 
 function App() {
   return (
     <div className='App'>
       <PersonAdd />
       <PersonList />
+      <PersonRemove />
     </div>
   );
 }
